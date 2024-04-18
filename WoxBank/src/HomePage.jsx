847b380e214c363fd173e4f781c0a9188491a909
Christokuriyan <christokuriyan87@gmail.com>
2024-04-18 15:06:18 +0530

    fb

--- WoxBank/src/HomePage.jsx
+++ WoxBank/src/HomePage.jsx
@@ -1,71 +1,38 @@
-
-
-// import RegForm from "../src/components/Reg_form";
-
-// const HomePage = () => {
-//   return (
-//     <div className="bg-green-200 flex">
-//       {/* First section */}
-//       <div className="flex-1 relative">
-//         {/* Background image */}
-//         <img src="../public/assets/registrations/new.png" alt="Background Image" className="absolute inset-0 w-full h-full object-cover opacity-10" />
-//         {/* Content in the first section */}
-//         <div className="min-h-screen flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8 text-black">
-//           <h1 className="text-4xl font-bold mb-4">Experience
-// hassle-free banking</h1>
-//           <p className="text-lg">Experience simple, secure, and stress-free banking. Say goodbye to long queues and complex procedures and hello to hassle-free banking with Wox Bank.</p>
-//         </div>
-//       </div>
-//       {/* Second section */}
-//       <div className="flex-1 relative">
-//         {/* Background image with decreased opacity */}
-//         <img src="../public/assets/registrations/new.png" alt="Background Image" className="absolute inset-0 w-full h-full object-cover opacity-10" />
-//         {/* Registration form */}
-//         <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-//           <RegForm />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HomePage;
-
 import RegForm from "../src/components/Reg_form";
 
 const HomePage = () => {
   return (
-    <div className="bg-green-200 flex">
+    <div className="bg-[#D4F3E7] flex flex-col md:flex-row">
       {/* First section */}
-      <div className="flex-1 relative">
+      <div className="relative flex-1">
         {/* Background image */}
         <img src="../public/assets/registrations/new.png" alt="Background Image" className="absolute inset-0 w-full h-full object-cover opacity-10" />
         {/* Content in the first section */}
         <div className="min-h-screen flex flex-col justify-center py-12 px-4 sm:px-6 lg:px-8 text-black">
           {/* Small heading */}
           <div className="mb-4">
-            <p className="text-sm font-semibold">Wox Bank</p>
-           
+            <p className="text-sm font-semibold text-[#33B786]">Wox Bank</p>
           </div>
           {/* Main heading */}
           <h1 className="text-4xl font-bold mb-4">
-            Experience </h1>
-            <h1 className="text-4xl font-bold mb-4"> hassle-free banking
+            Experience
           </h1>
-          <p className="text-lg">Experience simple, secure, and stress-free banking. Say goodbye</p><p className="text-lg"> to long queues and complex procedures and hello to hassle-free
-           <p className="text-lg"></p>banking with Wox Bank.</p>
+          <h1 className="text-4xl font-bold mb-4">hassle-free banking</h1>
+          <p className="text-lg font-sans text-left mb-4"> {/* Added font-serif and text-left classes */}
+            Experience simple, secure, and stress-free banking. Say goodbye to long queues and complex procedures and hello to hassle-free banking with Wox Bank.
+          </p>
           {/* Social media icons */}
           <div className="flex mt-4">
-            <a href="#" className="mr-2"><img src="facebook_icon.png" alt="Facebook" className="w-6 h-6" /></a>
-            <a href="#" className="mr-2"><img src="instagram_icon.png" alt="Instagram" className="w-6 h-6" /></a>
-            <a href="#" className="mr-2"><img src="twitter_icon.png" alt="Twitter" className="w-6 h-6" /></a>
+            <a href="#" className="mr-2"><img src="../public/assets/icons/facebook 2.svg" alt="Facebook" className="w-6 h-6" /></a>
+            <a href="#" className="mr-2"><img src="../public/assets/icons/instagram 2.svg" alt="Instagram" className="w-6 h-6" /></a>
+            <a href="#" className="mr-2"><img src="../public/assets/icons/twitter 2.svg" alt="Twitter" className="w-6 h-6" /></a>
           </div>
         </div>
       </div>
       {/* Second section */}
-      <div className="flex-1 relative">
+      <div className="relative flex-1">
         {/* Background image with decreased opacity */}
-        <img src="../public/assets/registrations/new.png" alt="Background Image" className="absolute inset-0 w-full h-full object-cover opacity-10" />
+        <img src="../public/assets/registrations/new.png" alt="Background Image" className="absolute inset-0 w-full h-full object-cover opacity-10 hidden md:block" />
         {/* Registration form */}
         <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <RegForm />
--- WoxBank/src/components/Reg_form.jsx
+++ WoxBank/src/components/Reg_form.jsx
@@ -1,202 +1,189 @@
 
 
+// import Button from "./Button";
+
+// const RegForm = () => {
+//   return (
+//     <div className="max-w-md w-full space-y-8" style={{ boxShadow: '0px 0px 150px 25px rgba(51, 183, 134, 0.25)' }}> {/* Added inline style for box shadow */}
+//       <div className="relative rounded-lg bg-white p-8 w-787 h-760 top-160 left-1013 gap-0 border-0 border-opacity-0">
+//         <h2 className="mt-6 text-left text-2xl font-bold text-[#33B786]">
+//           Register
+//         </h2>
+//         <form className="mt-8 space-y-6" action="#" method="POST">
+//           <div>
+//             <h3 className="text-sm font-medium text-gray-700">Name</h3>
+//             <label htmlFor="name" className="sr-only">
+//               Name
+//             </label>
+//             <input
+//               id="name"
+//               name="name"
+//               type="text"
+//               autoComplete="name"
+//               required
+//               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-[#33B786] focus:border-[#33B786] focus:z-10 sm:text-sm mb-4" // Added mb-4 for bottom margin
+//               placeholder="Name"
+//             />
+//           </div>
+//           <div>
+//             <h3 className="text-sm font-medium text-gray-700">Email</h3>
+//             <label htmlFor="email" className="sr-only">
+//               Email
+//             </label>
+//             <input
+//               id="email"
+//               name="email"
+//               type="email"
+//               autoComplete="email"
+//               required
+//               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#33B786] focus:border-[#33B786] focus:z-10 sm:text-sm mb-4" // Added mb-4 for bottom margin
+//               placeholder="Email"
+//             />
+//           </div>
+//           <div>
+//             <h3 className="text-sm font-medium text-gray-700">Password</h3>
+//             <label htmlFor="password" className="sr-only">
+//               Password
+//             </label>
+//             <input
+//               id="password"
+//               name="password"
+//               type="password"
+//               autoComplete="new-password"
+//               required
+//               className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-[#33B786] focus:border-indigo-[#33B786] focus:z-10 sm:text-sm mb-4" // Added mb-4 for bottom margin
+//               placeholder="Password"
+//             />
+//           </div>
+//           <div className="flex items-center">
+//             <input
+//               id="terms"
+//               name="terms"
+//               type="checkbox"
+//               className="h-4 w-4 text-[#33B786] focus:ring-[#33B786] border-gray-300 rounded"
+//             />
+//             <label htmlFor="terms" className="ml-2 block text-sm text-gray-900">
+//               I agree to all the Terms, Privacy Policy, and Fees
+//             </label>
+//           </div>
+
+//           <div>
+//             <Button>Register</Button>
+//           </div>
+//         </form>
+//         <p className="mt-2 text-center text-sm text-gray-600">
+//           Already have an account?{" "}
+//           <a href="#" className="font-medium text-[#33B786] hover:text-[#33B786]">
+//             Log in
+//           </a>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RegForm;
+
 import Button from "./Button";
 
 const RegForm = () => {
   return (
-    
-      <div className="max-w-md w-full space-y-8">
-        <div className="relative rounded-lg bg-white p-8 w-787 h-760 top-160 left-1013 gap-0 border-0 border-opacity-0">
-          <h2 className="mt-6 text-left text-2xl font-bold text-[#33B786]">
-            Register
-          </h2>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            
-              <div>
-                <h3 className="text-sm font-medium text-gray-700">Name</h3>
-                <label htmlFor="name" className="sr-only">
-                  Name
-                </label>
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  autoComplete="name"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-[#33B786] focus:border-[#33B786] focus:z-10 sm:text-sm mb-4" // Added mb-4 for bottom margin
-                  placeholder="Name"
-                />
-              </div>
-              <div>
-                <h3 className="text-sm font-medium text-gray-700">Email</h3>
-                <label htmlFor="email" className="sr-only">
-                  Email
-                </label>
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#33B786] focus:border-[#33B786] focus:z-10 sm:text-sm mb-4" // Added mb-4 for bottom margin
-                  placeholder="Email"
-                />
-              </div>
-              <div>
-                <h3 className="text-sm font-medium text-gray-700">Password</h3>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="new-password"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-[#33B786] focus:border-indigo-[#33B786] focus:z-10 sm:text-sm mb-4" // Added mb-4 for bottom margin
-                  placeholder="Password"
-                />
-              </div>
-              <div className="flex items-center">
-                <input
-                  id="terms"
-                  name="terms"
-                  type="checkbox"
-                  className="h-4 w-4 text-[#33B786] focus:ring-[#33B786] border-gray-300 rounded"
-                />
-                <label htmlFor="terms" className="ml-2 block text-sm text-gray-900">
-                  I agree to all the Terms, Privacy Policy, and Fees
-                </label>
-              </div>
-           
-
-            <div>
-              <Button>Register</Button>
+    <div className="max-w-md w-full space-y-8" style={{ boxShadow: '0px 0px 150px 25px rgba(51, 183, 134, 0.25)' }}>
+      <div className="relative rounded-lg bg-white p-8">
+        <h2 className="mt-6 text-left text-2xl font-bold text-[#33B786]">
+          Register
+        </h2>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <div className="relative">
+            <h3 className="text-sm font-medium text-gray-700">Name</h3>
+            <label htmlFor="name" className="sr-only">
+              Name
+            </label>
+            <div className="flex">
+              <span className="absolute left-3 top-3">
+                <img src="../path_to_icon/name_icon.svg" alt="Name" className="w-5 h-5" />
+              </span>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                autoComplete="name"
+                required
+                className="appearance-none rounded-none pl-10 relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-[#33B786] focus:border-[#33B786] focus:z-10 sm:text-sm mb-4"
+                placeholder="Name"
+              />
             </div>
-          </form>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Already have an account?{" "}
-            <a href="#" className="font-medium text-[#33B786] hover:text-[#33B786]">
-              Log in
-            </a>
-          </p>
-        </div>
+          </div>
+          {/* Similar structure for other input fields */}
+          <div className="relative">
+            <h3 className="text-sm font-medium text-gray-700">Email</h3>
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
+            <div className="flex">
+              <span className="absolute left-3 top-3">
+                <img src="../path_to_icon/email_icon.svg" alt="Email" className="w-5 h-5" />
+              </span>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none pl-10 relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#33B786] focus:border-[#33B786] focus:z-10 sm:text-sm mb-4"
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          {/* Similar structure for other input fields */}
+          <div className="relative">
+            <h3 className="text-sm font-medium text-gray-700">Password</h3>
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <div className="flex">
+              <span className="absolute left-3 top-3">
+                <img src="../path_to_icon/password_icon.svg" alt="Password" className="w-5 h-5" />
+              </span>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="new-password"
+                required
+                className="appearance-none rounded-none pl-10 relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-[#33B786] focus:border-indigo-[#33B786] focus:z-10 sm:text-sm mb-4"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          {/* Checkbox */}
+          <div className="flex items-center">
+            <input
+              id="terms"
+              name="terms"
+              type="checkbox"
+              className="h-4 w-4 text-[#33B786] focus:ring-[#33B786] border-gray-300 rounded"
+            />
+            <label htmlFor="terms" className="ml-2 block text-sm text-gray-900">
+              I agree to all the Terms, Privacy Policy, and Fees
+            </label>
+          </div>
+          {/* Register Button */}
+          <div>
+            <Button>Register</Button>
+          </div>
+        </form>
+        {/* Already have an account */}
+        <p className="mt-2 text-center text-sm text-gray-600">
+          Already have an account?{" "}
+          <a href="#" className="font-medium text-[#33B786] hover:text-[#33B786]">
+            Log in
+          </a>
+        </p>
       </div>
-    
+    </div>
   );
 };
 
 export default RegForm;
 
-
-
-// import React from "react";
-// import Button from "./Button";
-
-// const RegForm = () => {
-//     return (
-//         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-//             <div className="max-w-md w-full space-y-8">
-//                 <div className="relative rounded-lg bg-white p-8 w-787 h-760 top-160 left-1013 gap-0 border-0 border-opacity-0">
-//                     <h2 className="mt-6 text-left text-2xl font-bold text-[#33B786]">
-//                         Register
-//                     </h2>
-//                     <form className="mt-8 space-y-6" action="#" method="POST">
-//                         <div>
-//                             <div className="mb-5"> {/* Add margin-bottom for one-line gap */}
-//                                 <h3 className="text-sm font-medium text-gray-700">Name</h3>
-//                                 <div className="flex items-center border border-gray-300 rounded-md">
-//                                     <input
-//                                         id="name"
-//                                         name="name"
-//                                         type="text"
-//                                         autoComplete="name"
-//                                         required
-//                                         className="appearance-none relative block w-full px-3 py-2 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#33B786] focus:border-[#33B786] focus:z-10 sm:text-sm"
-//                                         placeholder="Name"
-//                                     />
-//                                     <img src="../../public/assets/registrations/fi-rr-user.svg" alt="User Icon" className="h-6 w-6 text-gray-400 mr-2" />
-//                                 </div>
-//                             </div>
-//                             <div className="mb-5"> {/* Add margin-bottom for one-line gap */}
-//                                 <h3 className="text-sm font-medium text-gray-700">Email</h3>
-//                                 <div className="flex items-center border border-gray-300 rounded-[5px]">
-//                                     <input
-//                                         id="email"
-//                                         name="email"
-//                                         type="email"
-//                                         autoComplete="email"
-//                                         required
-//                                         className="bg-white ml-2"
-//                                         placeholder="Email"
-//                                     />
-//                                     <img src="../../public/assets/registrations/Vector.svg" alt="Message Icon" className="h-6 w-6 text-gray-400 mr-2" />
-//                                 </div>
-//                             </div>
-//                             <div className="mb-25 "> {/* Add margin-bottom for one-line gap */}
-//                                 <h3 className="text-sm font-medium text-gray-700">Password</h3>
-//                                 <div className="flex items-center border border-gray-300 rounded-md">
-//                                     <input
-//                                         id="password"
-//                                         name="password"
-//                                         type="password"
-//                                         autoComplete="new-password"
-//                                         required
-//                                         className="appearance-none relative block w-full px-3 py-2 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-[#33B786] focus:border-[#33B786] focus:z-10 sm:text-sm"
-//                                         placeholder="Password"
-//                                     />
-//                                     <img src="../../public/assets/registrations/fi-rr-lock.svg" alt="Lock Icon" className="h-6 w-6 text-gray-400 mr-2" />
-//                                 </div>
-//                             </div>
-//                             <div className="flex items-center">
-//                                 <input
-//                                     id="terms"
-//                                     name="terms"
-//                                     type="checkbox"
-//                                     className="h-4 w-4 text-[#33B786] focus:ring-[#33B786] border-gray-300 rounded"
-//                                 />
-//                                 <label htmlFor="terms" className="ml-2 block text-sm text-gray-900 rounded-md">
-//                                     I agree to all the Terms, Privacy Policy, and Fees
-//                                 </label>
-//                             </div>
-//                         </div>
-
-//                         <div>
-//                             <Button>Register</Button>
-//                         </div>
-//                     </form>
-//                     <p className="mt-2 text-center text-sm text-gray-600">
-//                         Already have an account?{" "}
-//                         <a href="#" className="font-medium text-[#33B786] hover:text-[#33B786]">
-//                             Log in
-//                         </a>
-//                     </p>
-//                 </div>
-//             </div>
-
-
-
-
-
-
-//             <div>
-//                 <input
-//                     id="name"
-//                     name="name"
-//                     type="text"
-//                     autoComplete="name"
-//                     required
-//                     className="bg-white "
-//                     placeholder="Name"
-//                 />
-
-//             </div>
-
-
-
-
-//         </div>
-//     );
-// };
-
-// export default RegForm;
