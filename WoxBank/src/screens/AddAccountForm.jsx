18d334cbefd7553d6d4856795e3a5315b336ae6e
Christokuriyan <christokuriyan87@gmail.com>
2024-05-27 14:48:08 +0530

    g

--- WoxBank/src/screens/AddAccountForm.jsx
+++ WoxBank/src/screens/AddAccountForm.jsx
@@ -19,7 +19,7 @@ const AddAccountForm = ({isOpen, onRequestClose}) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission logic jn j h
+    // Handle form submission logic jn j h g
     console.log('Account Name:', accountName);
     console.log('Description:', description);
     // Navigate to another page if needed
